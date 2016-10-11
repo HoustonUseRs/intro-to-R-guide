15d6881ab4a37527f07f2ada88a92a6ed3b405b9
pandafulmanda <pandafulmanda@gmail.com>
2016-10-11 02:29:21 -0500

    move gh permissions stuffs earlier

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,16 @@ SHA=`git rev-parse --verify HEAD`
 git config --global user.name "Travis CI"
 git config --global user.email "$COMMIT_AUTHOR_EMAIL"
 
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in intro-to-r-deploy.enc -out intro-to-r-deploy -d
+chmod 600 intro-to-r-deploy
+eval `ssh-agent -s`
+ssh-add intro-to-r-deploy
+
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
 git clone $REPO out
@@ -49,16 +59,6 @@ doCompile
 # The delta will show diffs between new and old versions.
 git add _book
 git commit -m "Deploy to GitHub Pages: ${SHA}"
-
-# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in intro-to-r-deploy.enc -out intro-to-r-deploy -d
-chmod 600 intro-to-r-deploy
-eval `ssh-agent -s`
-ssh-add intro-to-r-deploy
 
 # Now that we're all set up, we can push.
 git subtree push --squash --prefix _book $SSH_REPO $TARGET_BRANCH
