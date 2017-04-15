6323bc6fd5ecef7f4c7e468c0b1adfcd4e51d6bc
amanda <pandafulmanda@gmail.com>
2017-04-15 06:29:09 -0500

    update example script to match changed content

--- example_script.R
+++ example_script.R
@@ -1,60 +1,41 @@
+#' # Analyzing air quality
 
-# comment out installation of packages
-# each time source is called
-#install.packages("ggplot2")
-#install.packages("dplyr")
-
-# BONUS
-# load and install libraries in a single function
-# p_load function, loads library if it is already installed,
-# if not it first installs, and then loads the library.
-#library(pacman)
-#p_load(ggplot2)
-#p_load(dplyr)
-
-# load libraries
+#' load libraries
 library(ggplot2)
 library(dplyr)
+library(broom)
 
-#' ## Read in data, on air quality metrics
+#' Read in data, on air quality metrics
 airquality <- read.csv('airquality.csv')
 
-#' ## filter for only complete cases
-aircomplete <- airquality[complete.cases(airquality), ]
+#' Filter for only complete cases
+aircomplete <- airquality %>% filter(complete.cases(airquality))
 
-#' ## create a plot comparing, Ozone levels with Temperature
-plt <- ggplot(aircomplete) + geom_point(aes(x=Temp, y=Ozone, color = Solar.R))
-print(plt)
+#' Create a plot comparing, Ozone levels with Temperature
+p <- ggplot(airquality) + geom_point(aes(x=Temp, y=Ozone))
+print(p)
 
-# let change the colors of solar radiation, and save it in 
-# a new variable
-plt = plt + scale_color_gradient(low = "orange", high = "red")
+#' Let's change the colors of solar radiation, and save it in a new variable
+p <- p + scale_color_gradient(low = "orange", high = "red")
 
-#' ## run a linear model 
-# ozone = a + bT + e
+#' Let's run a linear model 
+#' ozone = a + bT + e
 ozone_model <- lm(Ozone ~ Temp, data = aircomplete)
+ozone_model %>% summary()
 
-#' ## summarize the linear model
-print(summary(ozone_model))
+#' Are there any other parameters influencing ozone?
+two_model <- lm(Ozone ~ Temp + Solar.R, data = aircomplete)
+two_model %>% summary()
 
-# extrating coefficients
-coef(ozone_model)
+#' Extract the coefficients
+ozone_intercept <- ozone_model %>% tidy %>% filter(term == "(Intercept)") %>% select(estimate) %>% unlist()
+ozone_slope <- ozone_model %>% tidy %>% filter(term == "Temp") %>% select(estimate) %>% unlist()
 
-#' ## add a new layer
-plt_2 <- plt + geom_abline(intercept = -147.64607 , slope = 2.43911)
-print(plt_2)
-
-
-#' # Bonus
-aircomplete = mutate(aircomplete, temp2 = ifelse(Temp > 80, "high", "low"))
-ggplot(aircomplete) + geom_boxplot(aes(x=temp2, y=Ozone))
-
-#' We see that low shows up after high on the x axis, and it is counter intuitive.
-#' So we can use factors, as explained in the previous section to switch their position.
-aircomplete = mutate(aircomplete, temp_fac = factor(temp2, levels = c("low", "high")))
-ggplot(aircomplete) + geom_boxplot(aes(x=temp_fac, y=Ozone))
+#' Plot the linear model
+p2 <- p + geom_abline(intercept = ozone_intercept , slope = ozone_slope)
+print(p2)
 
 
-
-
-# END
+#' # Boxplot
+aircomplete <- aircomplete %>% mutate(Temp2 = ifelse(Temp < 80, "cool", "warm"))
+ggplot(aircomplete) + geom_boxplot(aes(x=Temp2, y=Ozone))
